ee6f25848a8ff0885df47862044bd5fccf4bbf1f
Oleh <oleh@basal.dev>
2025-03-07 09:18:39 +0900

    ✨ feature: Improve invite user email template URL formatting

--- src/emails/templates/inviteUser.ts
+++ src/emails/templates/inviteUser.ts
@@ -5,7 +5,7 @@ export const inviteUserTemplate = {
     bodyTemplate: `
       <p>Geekleをご利用いただきありがとうございます。</p>
       <p>招待が届いています。下記のURLより登録を行なってください。</p>
-      <p><a href="\${url}">\${url}</a></p>
+      <p>[<a href="\${url}">アカウント作成URL</a>]</p>
       <p>*※本メールは送信専用のメールアドレスで送信しております。</p>
       <p>本メールにご返信いただいてもお答えできません。あらかじめご了承ください。</p>
       <br />
@@ -22,7 +22,7 @@ export const inviteUserTemplate = {
     bodyTemplate: `
       <p>Geekleをご利用いただきありがとうございます。</p>
       <p>以下のリンクからアカウント作成を行ってください</p>
-      <p><a href="\${url}">\${url}</a></p>
+      <p>[<a href="\${url}">アカウント作成URL</a>]</p>
       <p>本メールは送信専用のアドレスです。</p>
       <p>このメールに心当たりがない場合は、お問い合わせください。</p>
       <br />
